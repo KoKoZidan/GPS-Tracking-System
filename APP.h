34e3942d318026c6e9a6580a0cedff6060ede809
maimoostafa <168725037+maimoostafa@users.noreply.github.com>
2024-05-16 20:49:49 +0300

    Update APP.h

--- APP.h
+++ APP.h
@@ -1,19 +1,17 @@
 #ifndef APP_H_
 #define APP_H_
 
-#include "../../Utilities/bitManipulation.h"
-#include "../../Utilities/tm4c123gh6pm.h"
-#include "../../MCAL/GPIO/GPIO.h"
-#include "../../MCAL/UART/UART.h"
-#include "../../MCAL/SYSTICK/Systick.h"
-//#include "../MCAL/sysctl/sysctl.h"
-//#include "../MCAL/sysctl/hw_ints.h"
-//#include "driverlib/sysctl.h"
-//#include "driverlib/inc/hw_ints.h"
-#include "../../HAL/GPS/GPS.h"
-#include "../../HAL/LED/LED.h"
+#include "bitManipulation.h"
+#include "tm4c123gh6pm.h"
+#include "GPIO.h"
+#include "UART.h"
+#include "Systick.h"
+
+#include "GPS.h"
+#include "LED.h"
 #include "stdint.h"
 
 
 
 #endif
+//author mai mostafa
