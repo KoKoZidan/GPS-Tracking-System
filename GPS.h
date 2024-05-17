3b6997ae2958b46ce35c84703b31c8b9ffaa2214
Kawthar 2001974 <168728546+KoKoZidan@users.noreply.github.com>
2024-05-17 11:31:39 +0300

    Update GPS.h

--- GPS.h
+++ GPS.h
@@ -1,9 +1,12 @@
+/////////////%%%%%%%%%%%% Student Name :  Yassmine Saeed     %%%%%%%%%%%%/////////////
+/////////////%%%%%%%%%%%% Student ID   :  2101135         %%%%%%%%%%%%/////////////
+/////////////%%%%%%%%%%%% Team Number  :  17              %%%%%%%%%%%%/////////////
 #ifndef GPS_H_INCLUDED
 #define GPS_H_INCLUDED
 #include <stdio.h>
 #include <stdlib.h>
-#include "../../MCAL/UART/UART.h"
-#include "../../MCAL/SYSTICK/Systick.h"
+#include "UART.h"
+#include "Systick.h"
 #include <math.h>
 
 void readGPS(char * lat, char * log);   ///if it returns 0 then its working and 1 means didnt find gprmc and -1 means invalid data
